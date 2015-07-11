6820aa24567f6e676957c7510c87340c17675d5a
Ryan Finnie <ryan@finnie.org>
2015-07-11 14:51:59 -0700

    render updates

--- dsari/render.py
+++ dsari/render.py
@@ -23,6 +23,7 @@ def parse_args():
     parser = argparse.ArgumentParser(
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('--config-dir', '-c', type=str, default=utils.DEFAULT_CONFIG_DIR)
+    parser.add_argument('--regenerate', '-r', action='store_true')
     parser.add_argument('--debug', action='store_true')
     return parser.parse_args()
 
@@ -41,8 +42,6 @@ def main(argv):
     else:
         lh_console.setLevel(logging.INFO)
     logger.addHandler(lh_console)
-
-    ignore_jobs = ['test', 'test2', 'test3', 'test4']
 
     if 'template_dir' in config and config['template_dir']:
         loader = jinja2.ChoiceLoader(
@@ -63,8 +62,8 @@ def main(argv):
     jobs = {}
     jobs_written = []
     db_conn = sqlite3.connect(os.path.join(config['data_dir'], 'dsari.sqlite3'))
-    for (job_name, run_id, start_time, stop_time, exit_code, trigger_type, trigger_data) in db_conn.execute('SELECT job_name, run_id, start_time, stop_time, exit_code, trigger_type, trigger_data FROM runs'):
-        if job_name in ignore_jobs:
+    for (job_name, run_id, start_time, stop_time, exit_code, trigger_type, trigger_data) in db_conn.execute('SELECT job_name, run_id, start_time, stop_time, exit_code, trigger_type, trigger_data FROM runs ORDER BY start_time'):
+        if 'render_reports' in config['jobs'][job_name] and (not config['jobs'][job_name]['render_reports']):
             logger.debug('Ignoring %s %s' % (job_name, run_id))
             continue
         context = {
@@ -78,10 +77,17 @@ def main(argv):
         }
         runs.append(context)
         if job_name not in jobs:
-            jobs[job_name] = []
-        jobs[job_name].append(context)
+            jobs[job_name] = {
+                'runs': [],
+            }
+        jobs[job_name]['last_run'] = datetime.datetime.fromtimestamp(start_time)
+        jobs[job_name]['last_duration'] = datetime.datetime.fromtimestamp(stop_time) - datetime.datetime.fromtimestamp(start_time)
+        if exit_code == 0:
+            jobs[job_name]['last_successful_run'] = datetime.datetime.fromtimestamp(start_time)
+        jobs[job_name]['runs'].append(context)
         if os.path.isfile(os.path.join(config['data_dir'], 'html', job_name, run_id, 'index.html')):
-            continue
+            if not args.regenerate:
+                continue
         if not os.path.exists(os.path.join(config['data_dir'], 'html', job_name, run_id)):
             os.makedirs(os.path.join(config['data_dir'], 'html', job_name, run_id))
         if os.path.isfile(os.path.join(config['data_dir'], 'runs', job_name, '%s.output' % run_id)):
@@ -95,10 +101,11 @@ def main(argv):
 
     for job_name in jobs:
         if job_name not in jobs_written:
-            continue
+            if not args.regenerate:
+                continue
         context = {
             'job_name': job_name,
-            'runs': jobs[job_name],
+            'runs': jobs[job_name]['runs'],
         }
         if not os.path.exists(os.path.join(config['data_dir'], 'html', job_name)):
             os.makedirs(os.path.join(config['data_dir'], 'html', job_name))
@@ -107,10 +114,10 @@ def main(argv):
         with open(os.path.join(config['data_dir'], 'html', job_name, 'index.html'), 'w') as f:
             f.write(index_template.render(context))
 
-    if len(jobs_written) > 0:
+    if (len(jobs_written) > 0) or args.regenerate:
         context = {
             'runs': runs,
-            'jobs': jobs.keys(),
+            'jobs': jobs,
         }
         index_template = templates.get_template('index.html')
         logger.info('Writing %s' % os.path.join(config['data_dir'], 'html', 'index.html'))
