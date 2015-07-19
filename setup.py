6e996dc8d0187086e924f74dd5cd5cc2b9ad54b2
Ryan Finnie <ryan@finnie.org>
2015-07-19 17:56:34 +0000

    Setup updates

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ from distutils.core import setup
 setup(
     name='dsari',
     description='Do Something and Record It',
+    version='0.0.0',
     author='Ryan Finnie',
     author_email='ryan@finnie.org',
     url='https://github.com/rfinnie/dsari',
